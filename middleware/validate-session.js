c2b8cf124db7effeca1eca88f7e438b3f6e97523
JMI <ikolajm@gmail.com>
2021-02-08 12:04:25 -0500

    initial commit - dependencies listed and middleware ready for authentication

---
+++ middleware/validate-session.js
@@ -0,0 +1,19 @@
+module.exports = (req, res, next) => {
+    if (req.method == "OPTIONS") return next();
+
+    const token = req.headers.authorization;
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+        // FAIL - Bad token
+        if (err) {
+            req.errors = err;
+            return next();
+        }
+        
+        let user = await User.findOne({ where: { id: decodedToken.id } })
+        // FAIL - No user found
+        if (!user) throw 'err';
+        // PASS - User is set
+        req.user = user;
+        next();
+    })
+}
